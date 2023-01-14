24cfdf0b2d188201b6cda606e9d7ada763c4e941
Nico Berlee <nico.berlee@on2it.net>
2023-01-14 21:12:06 +0100

    feat: upgrade etcd 3.5.6

--- pkg/apis/etcd/v1beta2/cluster.go
+++ pkg/apis/etcd/v1beta2/cluster.go
@@ -24,7 +24,7 @@ import (
 
 const (
 	defaultRepository  = "gcr.io/etcd-development/etcd"
-	DefaultEtcdVersion = "v3.5.5"
+	DefaultEtcdVersion = "v3.5.6"
 )
 
 var (
@@ -87,10 +87,10 @@ type ClusterSpec struct {
 	// The etcd-operator will eventually make the etcd cluster version
 	// equal to the expected version.
 	//
-	// The version must follow the [semver](http://semver.org) format, prefixed with "v", for example "v3.5.5".
+	// The version must follow the [semver](http://semver.org) format, prefixed with "v", for example "v3.5.6".
 	// Only etcd released versions are supported: https://github.com/etcd-io/etcd/releases
 	//
-	// If version is not set, default is "v3.5.5".
+	// If version is not set, default is "v3.5.6".
 	Version string `json:"version,omitempty"`
 
 	// Paused is to pause the control of the operator for the etcd cluster.
